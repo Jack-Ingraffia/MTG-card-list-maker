b2edaeb69633b9b8a7f631e060d612f057ae4eba
Jack Sebastian Ingraffia <88952001+Jack-Ingraffia@users.noreply.github.com>
2025-03-25 13:08:00 -0700

    Create Instant.java

---
+++ Instant.java
@@ -0,0 +1,21 @@
+public class Instant extends Card {
+    private String useCase;
+
+    public Instant(String name, int ManaValue, String color, String text, String useCase) {
+        super(name, ManaValue, color, text);
+        this.useCase = useCase;
+    }
+
+    public String getUseCase() {
+        return useCase;
+    }
+
+    public void setUseCase(String useCase) {
+        this.useCase = useCase;
+    }
+
+    @Override
+    public String toString() {
+        return super.getName() + " is a " + super.getColor() + " instant " + useCase + ", with a mana value of " + super.getManaValue() + ". This card says, " + super.getText();
+    }
+}
